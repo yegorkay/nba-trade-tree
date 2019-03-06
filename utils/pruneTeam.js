8e0a9ee28ccb233e1d6822d2a041cdd4113d1780
yegorkay <yegor.kariaguine@soulfx.com>
2019-03-05 22:02:39 -0500

    :construction: maybe an idea on how to get teams per player

--- utils/pruneTeam.js
+++ utils/pruneTeam.js
@@ -13,10 +13,11 @@ const pruneTeam = (tradeArray) => {
   );
 };
 
-const testHtml = `<p class="transaction "><strong>June 23, 2011</strong>: As part of a 3-team trade, traded by the <a data-attr-from="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a> with <a href="/players/j/jacksst02.html">Stephen Jackson</a> and <a href="/players/l/livinsh01.html">Shaun Livingston</a> to the <a data-attr-to="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a>; the <a data-attr-from="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a> traded <a href="/players/m/maggeco01.html">Corey Maggette</a> to the <a data-attr-to="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a>; the <a data-attr-from="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a> traded <a href="/players/f/fredeji01.html">Jimmer Fredette</a> and <a href="/players/s/salmojo01.html">John Salmons</a> to the <a data-attr-to="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a>; the <a data-attr-from="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a> traded <a href="/players/b/biyombi01.html">Bismack Biyombo</a> to the <a data-attr-to="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a>; and  the <a data-attr-from="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a> traded <a href="/players/u/udrihbe01.html">Beno Udrih</a> to the <a data-attr-to="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a>.</p>`
+const testHtml = `<p class="transaction "><strong>June 23, 2011</strong>: As part of a 3-team trade, traded by the <a data-attr-from="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a> with <a href="/players/j/jacksst02.html">Stephen Jackson</a> and <a href="/players/l/livinsh01.html">Shaun Livingston</a> to the <a data-attr-to="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a>; the <a data-attr-from="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a> traded <a href="/players/m/maggeco01.html">Corey Maggette</a> to the <a data-attr-to="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a>; the <a data-attr-from="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a> traded <a href="/players/f/fredeji01.html">Jimmer Fredette</a> and <a href="/players/s/salmojo01.html">John Salmons</a> to the <a data-attr-to="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a>; the <a data-attr-from="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a> traded <a href="/players/b/biyombi01.html">Bismack Biyombo</a> to the <a data-attr-to="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a>; and  the <a data-attr-from="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a> traded <a href="/players/u/udrihbe01.html">Beno Udrih</a> to the <a data-attr-to="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a>.</p>`;
 
 // if tradeString contains teamsInvolved, do this stuff
-const teamsInvolved = $(testHtml).clone()
+const teamsInvolved = $(testHtml)
+  .clone()
   .find('strong:nth-child(1)')
   .remove()
   .end()
@@ -29,38 +30,50 @@ const teamsInvolved = $(testHtml).clone()
 
 const tradePartners = $(testHtml)
   .children('a[data-attr-to]')
-  .map(function () {
-    return getAbbr($(this).text())
+  .map(function() {
+    return getAbbr($(this).text());
   })
   .get();
 
-const tradedBy = getAbbr($(testHtml).children('a:nth-last-of-type(1)').text());
+const tradedBy = getAbbr(
+  $(testHtml)
+    .children('a:nth-last-of-type(1)')
+    .text()
+);
 
 const tradedPlayers = $(testHtml)
   .children('a:not(:nth-of-type(-n + 1))')
-  .map(function () {
+  .map(function() {
     return {
       name: $(this).text(),
-      playerId: getPlayerId($(this).attr('href')),
-    }
+      playerId: getPlayerId($(this).attr('href'))
+    };
   })
   .get();
 
 const allValues = _.uniqBy(tradedPlayers, 'name');
 
-console.log({
-  teamsInvolved,
-  tradedBy,
-  tradePartners: _.uniq(tradePartners),
-  // tradedPlayers: pruneTeam(tradedPlayers),
-  allValues
-})
+// console.log({
+//   teamsInvolved,
+//   tradedBy,
+//   tradePartners: _.uniq(tradePartners),
+//   // tradedPlayers: pruneTeam(tradedPlayers),
+//   allValues
+// });
 
+const getTeamIndexes = teamNames
+  .map((team) => {
+    return allValues.findIndex((el) => el.name === team.teamName);
+  })
+  .filter((value) => value !== -1)
+  .sort((a, b) => a - b);
 
-const getTeamIndexes = teamNames.map((team) => {
-  return allValues.findIndex((el) => el.name === team.teamName)
-}).filter((value) => value !== -1).sort((a, b) => a - b)
+/** We need to loop over/map this to make it dynamic */
+const mke = allValues.slice(0, 2);
+const cha = allValues.slice(3, 4);
+const sac = allValues.slice(8, allValues.length + 1);
 
-console.log(getTeamIndexes)
+console.log(getTeamIndexes);
+console.log({ mke, cha, sac });
 
 module.exports = pruneTeam;
