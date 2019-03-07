2b8d44137299eed821885a5eaa367c8f39cd3d03
Yegor <yegor.kariaguine@soulfx.com>
2019-03-07 15:05:41 -0500

    :construction: getting multiteam to show up in API

--- services/scrapeSinglePlayerTransaction.js
+++ services/scrapeSinglePlayerTransaction.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const $ = require('cheerio');
+const _ = require('lodash');
 const utils = require('../utils');
 
 const {
@@ -9,8 +10,30 @@ const {
   filterByPicks,
   getPlayerId,
   isCurrentYear,
-  fetchCurrentDraftPicks
+  fetchCurrentDraftPicks,
+  isMultiTeam,
+  chunkArrayByElement,
+  splitArray,
+  prunePlayers,
+  formatMultiTeam
 } = utils;
+
+const gLeague = 'G-League';
+
+/**
+ * @param {*} htmlString (our tradedString)
+ * @param {*} index (1 = status, 5 = allTeamsInvolved, 7 = status (multi-team))
+ * @return {*} string
+ */
+const splitTradeString = (htmlString, index) => {
+  return htmlString
+    .clone()
+    .find('strong:nth-child(1)')
+    .remove()
+    .end()
+    .text()
+    .split(' ')[index];
+};
 
 const scrapeSinglePlayerTransaction = async (playerUrl, playerTradeDate) => {
   let data = [];
@@ -22,21 +45,15 @@ const scrapeSinglePlayerTransaction = async (playerUrl, playerTradeDate) => {
   const html = await page.content();
 
   $(selector, html).each(function () {
+
     const tradeString = $(this).text();
-    const gLeague = 'G-League';
     const isGLeague = tradeString.indexOf(gLeague) !== -1;
 
-    const status = $(this)
-      .clone()
-      .find('strong:nth-child(1)')
-      .remove()
-      .end()
-      .text()
-      .split(' ')[1]
-      .toLowerCase();
+    const allTeamsInvolved = splitTradeString($(this), 5);
+    const isMultiTrade = isMultiTeam(allTeamsInvolved);
 
-    const notTraded =
-      status === 'drafted' || status === 'signed' || status === 'waived';
+    const status = splitTradeString($(this), isMultiTrade ? 7 : 1).toLowerCase();
+    const isNotTraded = status !== 'traded';
 
     const transactionDate = $(this)
       .children('strong:nth-child(1)')
@@ -46,27 +63,46 @@ const scrapeSinglePlayerTransaction = async (playerUrl, playerTradeDate) => {
       .children('strong:first-child + a')
       .text();
 
+    // this is the first issue we encounter when we have a multi team trade. It returns a string it cannot parse
+    // we are going to return an array and get the first element since it's cleaner than a ternary
+
     const tradedTo = $(this)
       .children('a[data-attr-to]')
-      .text();
+      .map(function () {
+        return $(this).text();
+      })
+      .get()[0];
 
     const tradedPlayers = $(this)
       .children('a:not(:nth-of-type(-n + 1))')
       .map(function () {
-        return {
+
+        const playerData = {
           name: $(this).text(),
           playerId: getPlayerId($(this).attr('href')),
-          tradedTo: notTraded ? '' : getAbbr(!isGLeague ? tradedBy : '')
-        };
+        }
+        // ! tradedTo is incorrect if more than one player is involved in 1 to 1 team trades
+        return isMultiTrade
+          ? playerData
+          : Object.assign(playerData, { tradedTo: isNotTraded ? '' : getAbbr(!isGLeague ? tradedBy : '') })
       })
       .get();
 
-    const allTradePieces = notTraded ? [] : pruneTeam(tradedPlayers);
+    const allTradePieces = isNotTraded ? [] : pruneTeam(tradedPlayers);
+
+    const isMultiTeamTradedPlayers = () => {
+      const chunkedValues = chunkArrayByElement(
+        splitArray(tradedPlayers, tradedBy),
+        'match'
+      );
+      const tradedToArray = prunePlayers(tradedPlayers);
+      return _.flatten(formatMultiTeam(chunkedValues, tradedToArray))
+    }
 
 
     if (!isGLeague) {
 
-      const tradedPicks = notTraded
+      const tradedPicks = isNotTraded
         ? []
         : filterByPicks(allTradePieces, tradeString, getAbbr(tradedTo));
 
@@ -74,8 +110,8 @@ const scrapeSinglePlayerTransaction = async (playerUrl, playerTradeDate) => {
         status,
         transactionDate,
         tradedBy: getAbbr(tradedBy),
-        tradedTo: notTraded ? '' : getAbbr(tradedTo),
-        tradedPlayers: pruneTradedPlayers(allTradePieces, tradedPicks),
+        tradedTo: isNotTraded ? '' : getAbbr(tradedTo),
+        tradedPlayers: isMultiTeam ? isMultiTeamTradedPlayers() : pruneTradedPlayers(allTradePieces, tradedPicks),
         tradedPicks: !isCurrentYear(transactionDate)
           ? tradedPicks
           : fetchCurrentDraftPicks(tradeString, transactionDate)
--- utils/pruneTeam.js
+++ utils/pruneTeam.js
@@ -20,8 +20,8 @@ const pruneTeam = (tradeArray) => {
   );
 };
 
-const testHtml = `<p class="transaction "><strong>June 23, 2011</strong>: As part of a 3-team trade, traded by the <a data-attr-from="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a> with <a href="/players/j/jacksst02.html">Stephen Jackson</a> and <a href="/players/l/livinsh01.html">Shaun Livingston</a> to the <a data-attr-to="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a>; the <a data-attr-from="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a> traded <a href="/players/m/maggeco01.html">Corey Maggette</a> to the <a data-attr-to="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a>; the <a data-attr-from="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a> traded <a href="/players/f/fredeji01.html">Jimmer Fredette</a> and <a href="/players/s/salmojo01.html">John Salmons</a> to the <a data-attr-to="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a>; the <a data-attr-from="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a> traded <a href="/players/b/biyombi01.html">Bismack Biyombo</a> to the <a data-attr-to="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a>; and  the <a data-attr-from="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a> traded <a href="/players/u/udrihbe01.html">Beno Udrih</a> to the <a data-attr-to="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a>.</p>`;
-
+// const testHtml = `<p class="transaction "><strong>June 23, 2011</strong>: As part of a 3-team trade, traded by the <a data-attr-from="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a> with <a href="/players/j/jacksst02.html">Stephen Jackson</a> and <a href="/players/l/livinsh01.html">Shaun Livingston</a> to the <a data-attr-to="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a>; the <a data-attr-from="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a> traded <a href="/players/m/maggeco01.html">Corey Maggette</a> to the <a data-attr-to="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a>; the <a data-attr-from="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a> traded <a href="/players/f/fredeji01.html">Jimmer Fredette</a> and <a href="/players/s/salmojo01.html">John Salmons</a> to the <a data-attr-to="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a>; the <a data-attr-from="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a> traded <a href="/players/b/biyombi01.html">Bismack Biyombo</a> to the <a data-attr-to="CHA" href="/teams/CHA/2011.html">Charlotte Bobcats</a>; and  the <a data-attr-from="SAC" href="/teams/SAC/2011.html">Sacramento Kings</a> traded <a href="/players/u/udrihbe01.html">Beno Udrih</a> to the <a data-attr-to="MIL" href="/teams/MIL/2011.html">Milwaukee Bucks</a>.</p>`;
+const testHtml = `<p class="transaction "><strong>February 6, 2019</strong>: Traded by the <a data-attr-from="LAC" href="/teams/LAC/2019.html">Los Angeles Clippers</a> with <a href="/players/m/marjabo01.html">Boban Marjanovic</a> and <a href="/players/s/scottmi01.html">Mike Scott</a> to the <a data-attr-to="PHI" href="/teams/PHI/2019.html">Philadelphia 76ers</a> for <a href="/players/c/chandwi01.html">Wilson Chandler</a>, <a href="/players/m/muscami01.html">Mike Muscala</a>, <a href="/players/s/shamela01.html">Landry Shamet</a>, a 2020 1st round draft pick, a 2021 1st round draft pick, a 2021 2nd round draft pick and a 2023 2nd round draft pick. (Pick is top-14 protected.) (Pick is MIA's 2021 1st-round pick.) (Pick is DET's 2021 2nd-round pick.) (Pick is DET's 2023 2nd-round pick.)</p>`;
 // if tradeString contains teamsInvolved, do this stuff
 const getTeamsInvolvedOrStatus = (tradeString, status = true) => {
   return $(tradeString)
@@ -47,14 +47,14 @@ const transactionDate = $(testHtml)
 
 const tradedTo = $(testHtml)
   .children('a[data-attr-to]')
-  .map(function() {
+  .map(function () {
     return $(this).text();
   })
   .get();
 
 const tradedPlayers = $(testHtml)
   .children('a:not(:nth-of-type(-n + 1))')
-  .map(function() {
+  .map(function () {
     return {
       name: $(this).text(),
       playerId: getPlayerId($(this).attr('href'))
@@ -69,13 +69,13 @@ const chunkedValues = chunkArrayByElement(
 
 const tradedToArray = prunePlayers(tradedPlayers);
 
-console.log({
-  isMultiTeam: isMultiTeam(teamsInvolved),
-  status,
-  transactionDate,
-  tradedBy: getAbbr(tradedBy),
-  tradedTo: getAbbr(tradedTo[0]),
-  tradedPlayers: _.flatten(formatMultiTeam(chunkedValues, tradedToArray))
-});
+// console.log({
+//   isMultiTeam: isMultiTeam(teamsInvolved),
+//   status,
+//   transactionDate,
+//   tradedBy: getAbbr(tradedBy),
+//   tradedTo: getAbbr(tradedTo[0]),
+//   tradedPlayers: _.flatten(formatMultiTeam(chunkedValues, tradedToArray))
+// });
 
 module.exports = pruneTeam;
