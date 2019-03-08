69026a384e0fd42af23e55c57a34a987588ad4ca
Yegor <yegor.kariaguine@soulfx.com>
2019-03-08 15:41:36 -0500

    :memo: finished documenting utils

--- utils/filterByPicks.js
+++ utils/filterByPicks.js
@@ -6,7 +6,7 @@
  */
 const matchDraftString = (tradeString, player) => {
   /** ex: `2013 2nd round draft pick (Alex Oriakhi` 
-   * (There is no closing bracket on purpose) */
+   * **(There is no closing bracket on purpose)** */
   const regex = new RegExp(
     `(\\b(19|20)\\d{2}\\b\\s([1-9]|[1-5][0-9]|60)(?:st|nd|rd|th)\\s(round draft pick))\\s(\\()(${player})`,
     'g'
--- utils/onetoOneTrade.js
+++ utils/onetoOneTrade.js
@@ -2,20 +2,35 @@ const $ = require('cheerio');
 const getAbbr = require('./getAbbr');
 const getPlayerId = require('./getPlayerId');
 
-const getPlayerObj = (data, tradedBy, tradedTo, isFirstHalf = true) => {
+
+/**
+ * Maps over data to get an array of players
+ * @param {*} data The data we are mapping
+ * @param {*} tradedBy The team our selected player was traded by
+ * @param {*} tradedTo The team our selected player was traded to
+ * @param {*} isTradedBy A boolean switch that determines whether the player was traded by a team, or for a team
+ * @return {*} Returns an array of player objects
+ */
+const getPlayerObj = (data, tradedBy, tradedTo, isTradedBy) => {
   return data
     .map(function () {
       return {
         name: $(this).text(),
         playerId: getPlayerId($(this).attr('href')),
-        tradedBy: getAbbr(isFirstHalf ? tradedBy : tradedTo),
-        tradedTo: getAbbr(isFirstHalf ? tradedTo : tradedBy)
+        tradedBy: getAbbr(isTradedBy ? tradedBy : tradedTo),
+        tradedTo: getAbbr(isTradedBy ? tradedTo : tradedBy)
       };
     })
     .get();
 };
 
-/** Returns proper array where correct tradedTo and tradedBy values are assigned  */
+/**
+ * Get the array of players where correct tradedTo and tradedBy values are assigned
+ * @param {*} tradeString The HTML transaction string
+ * @param {*} tradedBy The team our selected player was traded by
+ * @param {*} tradedTo The team our selected player was traded to
+ * @return {*} Returns an array of player objects
+ */
 const oneToOneTrade = (tradeString, tradedBy, tradedTo) => {
   const firstHalfData = tradeString
     .children('strong:first-child + a:first-of-type')
@@ -26,8 +41,10 @@ const oneToOneTrade = (tradeString, tradedBy, tradedTo) => {
   const firstHalf = getPlayerObj(firstHalfData, tradedBy, tradedTo, true);
   const secondHalf = getPlayerObj(secondHalfData, tradedBy, tradedTo, false);
 
-  // WE need to check if data even exists in the first half, otherwise, tradedTo/tradedBy are incorrect
-
+  /**
+   * WE need to check if data even exists in the first half, 
+   * otherwise, tradedTo/tradedBy are incorrect
+   */
   if (firstHalfData.text().length > 0) {
     return firstHalf.concat(secondHalf);
   } else {
