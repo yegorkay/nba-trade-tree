c054ffe2b7a133c95d0fca64c319081a9b1eb27a
Yegor <yegor.kariaguine@soulfx.com>
2019-03-14 15:27:30 -0400

    :construction: fixed sorting issue for teams

--- utils/getPicks.js
+++ utils/getPicks.js
@@ -39,9 +39,7 @@ const joinPicks = (tradeString, teamsInvolved, index) => {
   const assetMatch = tradeString.match(assetRegex);
 
   const assets = getAssets(assetMatch, teamsInvolved, index);
-
   if (playerMatch !== null) {
-
     const draftedPlayers = getDraftedPlayers(playerMatch, teamsInvolved, index);
     /** `Future` indicates that the player we are searching was drafted. Therefore, we don't want him in this data */
     const undraftedPicks = assets.filter((asset) =>
@@ -69,15 +67,27 @@ const splitString = (tradeString) => {
   );
 };
 
+// this returns the array in alphabetical order. We want to return it in order of appearance
 const getTeamsInString = (tradeString) => {
   let teams = [];
   for (let i = 0; i < teamNames.length; i++) {
     const team = teamNames[i].teamName;
     if (new RegExp(`\\b${team}\\b`, 'g').test(tradeString)) {
-      teams.push(teamNames[i].teamAbr);
+      /**
+       * might need to optimize this... I am seeing which index is first, sorting the array 
+       * by index size, then mapping it over to get back the teams I want in order...
+       */
+      teams.push({ abbr: teamNames[i].teamAbr, stringIndex: tradeString.indexOf(team) });
     }
   }
-  return teams;
+
+  const compareStringIndex = (a, b) => {
+    if (a.stringIndex < b.stringIndex) { return -1 };
+    if (a.stringIndex > b.stringIndex) { return 1 };
+    return 0;
+  }
+
+  return teams.sort(compareStringIndex).map((team) => team.abbr);
 };
 
 const getPicks = (tradeString) => {
@@ -86,7 +96,7 @@ const getPicks = (tradeString) => {
   const tradeFragment = splitString(tradeString);
   // if multiteam, we split string, otherwise we get teams in string
   const teamsInvolved = isMultiTeam ? getTeamsInString(tradeFragment) : getTeamsInString(tradeString);
-
+  // console.log(teamsInvolved)
   const mappedData = tradeFragment
     .map((tradeFragment, index) => joinPicks(tradeFragment, teamsInvolved, index))
     .filter((picksArray) => picksArray.length > 0);
@@ -99,7 +109,12 @@ const ay = `February 6, 2019: Traded by the Los Angeles Clippers with Boban Marj
 
 // this is returning incorrect tradeBy/tradedTo
 const hard = `October 27, 2012: Traded by the Oklahoma City Thunder with Cole Aldrich, Daequan Cook and Lazar Hayward to the Houston Rockets for Jeremy Lamb, Kevin Martin, a 2013 1st round draft pick (Steven Adams was later selected), a 2013 2nd round draft pick (Alex Abrines was later selected) and a 2014 1st round draft pick (Mitch McGary was later selected).`;
+const kawhi = `July 18, 2018: Traded by the San Antonio Spurs with Danny Green and cash to the Toronto Raptors for DeMar DeRozan, Jakob Poeltl and a 2019 1st round draft pick. (2019 1st-rd pick is top-20 protected.) $5MM`;
+const toby = `February 6, 2019: Traded by the Los Angeles Clippers with Boban Marjanovic and Mike Scott to the Philadelphia 76ers for Wilson Chandler, Mike Muscala, Landry Shamet, a 2020 1st round draft pick, a 2021 1st round draft pick, a 2021 2nd round draft pick and a 2023 2nd round draft pick. (Pick is top-14 protected.) (Pick is MIA's 2021 1st-round pick.) (Pick is DET's 2021 2nd-round pick.) (Pick is DET's 2023 2nd-round pick.)`;
+const toby2 = `January 29, 2018: Traded by the Detroit Pistons with Avery Bradley, Boban Marjanovic, a 2018 1st round draft pick (Miles Bridges was later selected) and a 2019 2nd round draft pick to the Los Angeles Clippers for Blake Griffin, Brice Johnson and Willie Reed.`;
 
-console.log(getPicks(test2))
+const alphaTest = `Traded by the Phoenix Suns to the Denver Nuggets`
+
+console.log(getPicks(kawhi))
 
 module.exports = getPicks;
