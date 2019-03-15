6376d52a9950e5f17ea96ca84a81a578e202ca41
Yegor <yegor.kariaguine@soulfx.com>
2019-03-15 14:50:53 -0400

    :construction: more conversion

---
+++ src/services/scrapeMainTransaction.ts
@@ -0,0 +1,55 @@
+import puppeteer from 'puppeteer';
+import $ from "cheerio";
+import { bballPrefix } from './../settings';
+
+interface ITrade {
+  player: string;
+  prevTeam: string;
+  currTeam: string;
+  link: string;
+  tradeDate: string;
+}
+
+export const scrapeMainTransaction = async (f1: string, f2: string, tradeTableIndex: number = 0) => {
+  let data: ITrade[] = [];
+  let tradeDates: string[] = [];
+  const selector = `#st_${tradeTableIndex} tr`;
+  const tradeURL = `${bballPrefix}/friv/trades.fcgi?f1=${f1}&f2=${f2}`;
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(tradeURL);
+  const html = await page.content();
+
+  $('p.transaction strong', html).each((_i: number, ele: CheerioElement) => {
+    const date = $(ele).text();
+    tradeDates.push(date);
+  });
+
+  $(selector, html)
+    .not('.thead')
+    .each((_i: number, ele: CheerioElement) => {
+      const child = (index: number): string =>
+        $(ele)
+          .children(`td:nth-child(${index})`)
+          .text();
+      const suffix: string = $(ele)
+        .children(`td:nth-child(1)`)
+        .children('a')
+        .attr('href');
+      const player: string = child(1);
+      const prevTeam: string = child(2);
+      const currTeam: string = child(10);
+      data.push({
+        player,
+        prevTeam,
+        currTeam,
+        link: `${bballPrefix}${suffix}#all_transactions`,
+        tradeDate: tradeDates[tradeTableIndex]
+      });
+    });
+
+  await browser.close();
+
+  return data;
+};
--- src/services/scrapeTeamCodes.ts
+++ src/services/scrapeTeamCodes.ts
@@ -1,26 +1,27 @@
 import puppeteer from 'puppeteer';
 import $ from "cheerio";
+import { ITeam } from './../models';
 
 export const scrapeTeamCodes = async () => {
-  let data: any = [];
-  const rowSelector = 'tr:not(:first-child)';
+  let data: ITeam[] = [];
+  const rowSelector: string = 'tr:not(:first-child)';
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(
     'https://en.wikipedia.org/wiki/Wikipedia:WikiProject_National_Basketball_Association/National_Basketball_Association_team_abbreviations'
   );
-  const html = await page.content();
+  const html: string = await page.content();
 
-  $(rowSelector, html).each((i, ele) => {
-    const selector = (index: number) =>
+  $(rowSelector, html).each((_i: number, ele: CheerioElement) => {
+    const selector = (index: number): string =>
       $(ele)
         .children(`td:nth-child(${index})`)
         .text()
         .replace('\n', '');
 
-    const teamAbr = selector(1);
-    const teamName = selector(2);
+    const teamAbr: string = selector(1);
+    const teamName: string = selector(2);
 
     data.push({ teamAbr, teamName });
   });
