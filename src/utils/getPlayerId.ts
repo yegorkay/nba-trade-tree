3ea971b0927228278a8e2f0235c90ef97fe7cd0a
Yegor <yegor.kariaguine@soulfx.com>
2019-03-15 16:08:17 -0400

    :bug: fixed getPlayerId

--- src/utils/getPlayerId.ts
+++ src/utils/getPlayerId.ts
@@ -5,12 +5,11 @@
  * @return Returns playerId `(/h/harrito02.html => harrito02)`
  */
 export const getPlayerId = (playerURL: string): string => {
-  // if (playerURL.length > 0) {
-  //   const str: string | undefined = playerURL.split('/').pop();
-  //   if (typeof str === 'string') {
-  //     return str.replace(/(.html)/g, '');
-  //   }
-  // }
-  // return '';
-  return playerURL;
+  if (playerURL !== undefined) {
+    const str: string | undefined = playerURL.split('/').pop();
+    if (typeof str === 'string') {
+      return str.replace(/(.html)/g, '');
+    }
+  }
+  return '';
 };
