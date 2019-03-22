2c14cc9cac151f0a52978b07a2a05f085ed02ff1
Yegor <yegor.kariaguine@soulfx.com>
2019-03-22 11:20:13 -0400

    :construction: nock transaction api

--- src/tests/scrapeSinglePlayerTransaction.test.ts
+++ src/tests/scrapeSinglePlayerTransaction.test.ts
@@ -1,9 +1,6 @@
-// import { scrapeSinglePlayerTransaction } from './../services';
 import nock from 'nock';
 
-jest.setTimeout(30000);
-
-test('test transaction API', () => {
+test('mock transaction API', () => {
   const playerId = 'hardeja01';
 
   nock('http://localhost')
@@ -11,17 +8,90 @@ test('test transaction API', () => {
     .reply(200, {
       data: [
         {
-          status: 'drafted',
-          transactionDate: 'June 25, 2009',
-          tradedBy: 'OKC',
-          tradedTo: '',
-          tradedPlayers: [],
-          tradedPicks: []
+          "status": "drafted",
+          "transactionDate": "June 25, 2009",
+          "tradedBy": "OKC",
+          "tradedTo": "",
+          "tradedPlayers": [],
+          "tradedPicks": []
+        },
+        {
+          "status": "traded",
+          "transactionDate": "October 27, 2012",
+          "tradedBy": "OKC",
+          "tradedTo": "HOU",
+          "tradedPlayers": [
+            {
+              "name": "Cole Aldrich",
+              "playerId": "aldrico01",
+              "tradedBy": "OKC",
+              "tradedTo": "HOU"
+            },
+            {
+              "name": "Daequan Cook",
+              "playerId": "cookda02",
+              "tradedBy": "OKC",
+              "tradedTo": "HOU"
+            },
+            {
+              "name": "Lazar Hayward",
+              "playerId": "haywala01",
+              "tradedBy": "OKC",
+              "tradedTo": "HOU"
+            },
+            {
+              "name": "Jeremy Lamb",
+              "playerId": "lambje01",
+              "tradedBy": "HOU",
+              "tradedTo": "OKC"
+            },
+            {
+              "name": "Kevin Martin",
+              "playerId": "martike02",
+              "tradedBy": "HOU",
+              "tradedTo": "OKC"
+            }
+          ],
+          "tradedPicks": [
+            {
+              "name": "Steven Adams",
+              "playerId": "adamsst01",
+              "tradedBy": "HOU",
+              "tradedTo": "OKC",
+              "pick": "2013 1st round draft pick"
+            },
+            {
+              "name": "Alex Abrines",
+              "playerId": "abrinal01",
+              "tradedBy": "HOU",
+              "tradedTo": "OKC",
+              "pick": "2013 2nd round draft pick"
+            },
+            {
+              "name": "Mitch McGary",
+              "playerId": "mcgarmi01",
+              "tradedBy": "HOU",
+              "tradedTo": "OKC",
+              "pick": "2014 1st round draft pick"
+            }
+          ]
+        },
+        {
+          "status": "signed",
+          "transactionDate": "July 9, 2016",
+          "tradedBy": "HOU",
+          "tradedTo": "",
+          "tradedPlayers": [],
+          "tradedPicks": []
+        },
+        {
+          "status": "signed",
+          "transactionDate": "July 8, 2017",
+          "tradedBy": "HOU",
+          "tradedTo": "",
+          "tradedPlayers": [],
+          "tradedPicks": []
         }
       ]
     });
-
-  // return scrapeSinglePlayerTransaction(playerId)
-  //   .then((data: any) => data)
-  //   .then((res: any) => expect(res).toEqual('GB'));
 });
